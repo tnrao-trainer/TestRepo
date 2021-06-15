1a6dbd10aed24e6b505993690cf1e97169567550
Narasimha Rao <tnrao.trainer@gmail.com>
2021-06-15 11:07:00 +0530

    My first changes to WebApiValues Services

---
+++ MyTestWebApiApplication/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyTestWebApiApplication.Models;
+
+namespace MyTestWebApiApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var prdList = new List<Product>() {
+                new Product(){ ProductId = 1022, ProductName = "Printer", UnitPrice = 2400, Quantity = 5 },
+                new Product(){ ProductId = 1023, ProductName = "Keyboard", UnitPrice = 1200, Quantity = 3 },
+                new Product(){ ProductId = 1024, ProductName = "iBall Mouse", UnitPrice = 890, Quantity = 4 },
+                new Product(){ ProductId = 1025, ProductName = "Boat Speakers", UnitPrice = 1900, Quantity = 6 },
+                new Product(){ ProductId = 1026, ProductName = "DLink Router", UnitPrice = 1350, Quantity = 2 }
+            };
+
+            return Ok(prdList);
+        }
+    }
+}
---
+++ MyTestWebApiApplication/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTestWebApiApplication.Models
+{
+    public class Product
+    {       
+        public int ProductId { get; set; }        
+        public string ProductName { get; set; }        
+        public int Quantity { get; set; }        
+        public double UnitPrice { get; set;  }
+    }
+}
